d16b0b862ba3307fb5a8c82e2f7e8884c435e1ff
N00rAhmed <tronn232003@gmail.com>
2023-04-25 12:55:41 +0100

    frontend folder

---
+++ src/components/TaskDetails.js
@@ -0,0 +1,26 @@
+import { useTasksContext } from "../hooks/useTasksContext";
+
+const TaskDetails = ({task}) => {
+    const { dispatch } = useTasksContext()
+    const handleClick = async () => {
+        const response = await fetch('/api/tasks/' + task._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if (response.ok){
+            dispatch({type: 'DELETE_TASK', payload: json})
+        }
+    }
+    return(
+        <div className="task-details">
+            <h4>{task.title}</h4>
+            <p><strong>description: </strong>{task.description}</p>
+            <p>{task.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
+        </div>
+
+    )
+}
+
+export default TaskDetails
