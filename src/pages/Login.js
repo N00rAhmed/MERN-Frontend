d18c553fc9e94c4c084db109e3927562dcfbbf57
N00rAhmed <tronn232003@gmail.com>
2023-11-06 12:29:53 +0000

    added loading

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,42 +1,52 @@
 import React from 'react';
-import { useState } from "react";
-import { useLogin } from "../hooks/useLogin"
+import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
 
 const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const {login, error, isLoading} = useLogin()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, error, isLoading } = useLogin();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(email, password);
+  };
 
-        await login(email, password)
-    }
+  return (
+    <form className="login" onSubmit={handleSubmit}>
+      <h3>Log in</h3>
 
-    return (
-        <form className="login" onSubmit={handleSubmit}>
-            <h3>Log in</h3>
+      <label>Email:</label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
 
-            <label>Email:</label>
-            <input type="email" 
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            />
+      <label>Password:</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
 
-            <label>password:</label>
-            <input type="password" 
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            />
+      <button disabled={isLoading}>Login</button>
 
-            <button disabled={isLoading}>Login</button>
+      {error && <div className="error">{error}</div>}
 
-            {error && <div className='error'>{error}</div>}
+      {isLoading && <LoadingSpinner />}
+    </form>
+  );
+};
 
-        </form>
-    )
+// LoadingSpinner component
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner">
+      <p>Loading...</p>
+      {/* You can add your spinner or loading animation here */}
+    </div>
+  );
+};
 
-}
-
-
-export default Login
+export default Login;
